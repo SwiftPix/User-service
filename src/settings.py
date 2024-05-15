3ab500dd543fdc8790000fbae944d05491d50161
CibeleG <cibelegoudinho13@gmail.com.br>
2024-05-14 22:46:29 -0300

    :repeat: Alterando nome padrão do banco de dados

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         self.ENVIROMENT = os.getenv("ENVIROMENT", "dev")
         self.MONGO_DATABASE_URI = os.getenv("MONGO_DATABASE_URI", "mongodb://localhost:27017")
-        self.MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME", "userDB")
+        self.MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME", "user-dev")
 
 settings = Settings()
