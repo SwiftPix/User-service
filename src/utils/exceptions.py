c482644c0b96d94364f252ccf9010706352aca55
Júlia Farias <julia.farias840@gmail.com>
2024-06-28 01:54:40 -0300

    :bulb: Criando exeção

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -22,4 +22,8 @@ class ValidationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
- 
+class UserNotFoundException(Exception):
+    pass
+
+class InvalidCredentialsException(Exception):
+    pass
